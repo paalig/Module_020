de3f3ae9ead1f701d2b4dbbb76c00a7263676670
Alex <alex0692pash@mail.ru>
2021-02-23 17:40:18 +0300

    003 init

---
+++ 003_Task3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+ * Написать функцию, которая принимаем указатель на char, по которому лежит строка.
+ * Функция должна возвращать true, если вторая строка является подстрокой первой.
+ * Пример:
+ * char* a = “Hello world”;
+ * char* b = “wor”;
+ * char* c = “banana”;
+ * std::cout << substr(a,b) << “ “ << substr(a,c);
+ * // true false
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
